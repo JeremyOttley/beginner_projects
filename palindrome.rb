9dfb4bdf5805a04c0687c3845f1e431c505ec323
Gazaunga <Gazaunga@gmail.com>
2018-03-02 07:44:56 -0500

    Create palindrome.rb

---
+++ palindrome.rb
@@ -0,0 +1,18 @@
+##Check if Palindrome – Checks if the string entered by the user is a palindrome. That is that it reads the same forwards as backwards like “racecar”
+## TODO
+# Convert 2 strs to arrays of chars
+# compare both arrays
+#
+
+#!/usr/bin/ruby -w
+
+"Give me a palindrome!"
+input = gets.chomp #=> "a nut for a jar of tuna"
+str = input.dup
+str.chars
+x === y
+
+##
+
+# frozen_string_literal: true
+str = gets.chomp
